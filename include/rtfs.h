389637247f0522a5de497594e29c1ea44036b41f
Tobias Derksen <t.derksen@gmx.de>
2017-11-22 13:07:19 +0100

    Started with key structure

--- include/rtfs.h
+++ include/rtfs.h
@@ -2,6 +2,26 @@
 #define RTFS_RTFS_H
 
 
+class Key {
+
+public:
+    explicit Key(unsigned long hash_) : hash(hash_) {}
+
+    unsigned long getFilehash() const noexcept  {
+        return hash & 0x00000000FFFFFFFF;
+    }
+
+    unsigned long getParenthash() const noexcept  {
+        return hash & 0xFFFFFFFF00000000;
+    }
+
+    unsigned long getHash() const noexcept  {
+        return hash;
+    }
+
+private:
+    unsigned long hash;
+};
 
 
 #endif //RTFS_RTFS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <bitset>
+
+#include "rtfs.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Key key(0xf0f0f0ff00f0f0f0);
+
+    cout << bitset<64>(key.getHash()) << endl;
+    cout << bitset<64>(key.getParenthash()) << endl;
+    cout << bitset<64>(key.getFilehash()) << endl;
+
     return 0;
 }
